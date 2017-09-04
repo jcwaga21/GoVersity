52fd478bdb9376eee32cebdb4e174f7f28cbf382
jcwaga21 <johnclyde.waga@g.msuiit.edu.ph>
2017-09-04 21:34:46 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="main">
+	<div class="topnav">
+		<div class="logo">
+			<ul>
+				<li><a href="#"><img src="logo.png"></a></li>
+				<li><p><span id="space">Mindanao State University</span> <br> <span id="small">ILIGAN INSTITUTE OF TECHNOLOGY</span></p></li>
+			</ul>
+		</div>
+			<nav id="toplog">
+				<ul>
+					<li><a href="#">Login</a></li>
+					<li><a href="#">Sign Up</a></li>
+				</ul>
+			</nav>
+		</div>
+		<div class="cover">
+			<div class="tagline">
+				<h2>Welcome to <span class="highlight">We</span>App</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean iaculis luctus urna, ut faucibus leo porttitor tristique. Suspendisse vel tempor nisl, eget lacinia erat</p>
+			</div>
+				<div class="container">
+					<form>
+					<img src="logo1.png" id="logo">
+					<h3>Sign in to connect with your friends</h3>
+					<input type="text" placeholder="Username" name="	Username"><br>
+					<input type="password" placeholder="Password" name="password"><br>
+					<button><a href="#" id="login">Login</a></button>
+					<p>Dont have an account? <br> <a href="#" id="signup">Sign Up</a></p>
+				</form>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div class="footer">
+		<ul>
+			<li><p>Mindanao State University<br>Iligan Institute of Technology</p></li>
+			<li><div id="line"></div></li>
+			<li><p>Andres Bonifacio Avenue, Tibanga, <br>
+				9200 Iligan City, Philippines<br>
+				Telephone: +63 (063) 492 1173</p></li>
+		</ul>
+		</div>
+		<div id="lower">
+			<ul>
+				<li><a href="#">ABOUT US</a></li>
+				<li><a href="#">PRIVACY STATEMENT</a></li>
+				<li><a href="#">COPYRIGHT</a></li>
+				<li><a href="#">TERMS AND CONDITION</a></li>
+				<li><a href="#">DISCLAIMER</a></li>
+			</ul>
+		</div>
+	</footer>
+
+</body>
+</html>
